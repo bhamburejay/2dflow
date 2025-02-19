1e9918d0ac898f13eb5fef70cb8d75f6f10206e7
Jay Bhambure <jay.bhambure@stonybrook.edu>
2025-02-19 18:50:05 -0500

    input comes from 2dflow_input.json

--- work/2dflow.cpp
+++ work/2dflow.cpp
@@ -1,34 +1,56 @@
-// Note: The output of this file is stored in enery_out.h5
-// a python file called 2dflow_plot.py takes the h5 file as input to produce plots
+// Compile using CMake in build folder
+// The output of this file is stored in enery_out.h5 in build (NOTE TO SELF: move this to some place else)
+// 2dflow_plot.py takes the enery_out.h5 file as input to produce plots
 
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <mpi.h>
 #include <petsc.h>
+#include <petscdm.h>
 #include <petscdmda.h>
 #include <petscviewerhdf5.h>
-#include <cmath>
+#include <json/json.h>
 #include "VischydroNode.hpp"
-
 
 int main(int argc, char **argv) {
     PetscErrorCode ierr;
     PetscMPIInt rank;
-    DM da;
+    DM da;                                  // 2d grid object
     Vec solution, local_sol;
-    VischydroNode **nodes;  
+    VischydroNode **nodes;                  // Class that defined required nodes
+    PetscInt nx, ny;                        // latttice size
+    PetscReal Lx, Ly, E0, sigma, dx, dy;    // physical size, energy amplitude, std dev, grid spacing
     
-    // NOTE TO SELF: as a check make values non-uniform along x & y directions
-    PetscInt nx = 64, ny = 64;                       // grid size
-    PetscReal Lx = 1.0, Ly = 1.0;                    // Physical size
-    PetscReal dx = Lx/(nx-1), dy = Ly/(ny-1);dy;     // grid spacing
-    PetscReal sigma = 0.1;                           // gaussian width
-    PetscReal E0 = 1.0;                              // energy density amplitute
-
     ierr = PetscInitialize(&argc, &argv, NULL, NULL); CHKERRQ(ierr);
     ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank); CHKERRQ(ierr);
 
-    // 2d grid with ghosted boundary conditions
-    // this means, the values of the real boundary cells are copied to ghost cells
-    // micking neumann boundary conditions 
-    static const int stencil_width = 2;
+    // Json setup
+    Json::Value config;
+    std::ifstream config_file("2dflow_input.json");
+    Json::CharReaderBuilder reader;
+    std::string errs;
+
+    if (!Json::parseFromStream(reader, config_file, &config, &errs)) {
+        std::cerr << "JSON parse error: " << errs << std::endl;
+        PetscFinalize();
+        return 1;
+    }
+
+    // Extract parameters from JSON
+    nx = config["grid"]["nx"].asInt();
+    ny = config["grid"]["ny"].asInt();
+    static const int stencil_width = config["grid"]["stencil_width"].asInt();
+    Lx = config["physical_size"]["Lx"].asDouble();
+    Ly = config["physical_size"]["Ly"].asDouble();
+    E0 = config["initial_conditions"]["amplitude"].asDouble();
+    sigma = config["initial_conditions"]["sigma"].asDouble();
+
+    dx = Lx / (nx - 1);
+    dy = Ly / (ny - 1);
+
+    // 2d grid with ghosted boundary conditions -> this means, the values of the real boundary cells 
+    // are copied to ghost cells, micking neumann boundary conditions
     ierr = DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, DM_BOUNDARY_GHOSTED,
                         DMDA_STENCIL_BOX, nx, ny, PETSC_DECIDE, PETSC_DECIDE,
                         VischydroNode::NDOF, stencil_width,
@@ -36,38 +58,41 @@ int main(int argc, char **argv) {
     ierr = DMSetFromOptions(da); CHKERRQ(ierr);
     ierr = DMSetUp(da); CHKERRQ(ierr);
 
-    // Set coordinates 
+    // Set coordinates
+    ierr = DMDASetUniformCoordinates(da, -Lx / 2, Lx / 2, -Ly / 2, Ly / 2, 0, 0); CHKERRQ(ierr);
+    
     // global_vec (solutions) to store solution of diff. eqn. and 
     // local_vec (local_sol) part of global_vec to perform distributed computing at each process
-    ierr = DMDASetUniformCoordinates(da, -Lx/2, Lx/2, -Ly/2, Ly/2, 0, 0); CHKERRQ(ierr);
     ierr = DMCreateGlobalVector(da, &solution); CHKERRQ(ierr);
     ierr = DMCreateLocalVector(da, &local_sol); CHKERRQ(ierr);
 
-    // capture local grid info in a process
+    // capture local grid info in a given process
     PetscInt xs, ys, xm, ym;
     ierr = DMDAGetCorners(da, &xs, &ys, NULL, &xm, &ym, NULL); CHKERRQ(ierr);
-    
+
     // "lock" the local grid info in local_sol to populate /update it
     ierr = DMDAVecGetArray(da, local_sol, &nodes); CHKERRQ(ierr);
-    for(PetscInt j = ys; j < ys + ym; j++) {
-        for(PetscInt i = xs; i < xs + xm; i++) {
-            PetscReal x = (i - 0.5*(nx-1)) * dx;
-            PetscReal y = (j - 0.5*(ny-1)) * dy;
-            // NOTE TO SELF: Change the center & maybe even sigma_x and sigma_y 
-            // Also change momentum during next leg of progress
-            nodes[j][i].E = E0 * std::exp(-(x*x + y*y)/(2*sigma*sigma));
+
+    for (PetscInt j = ys; j < ys + ym; j++) {
+        for (PetscInt i = xs; i < xs + xm; i++) {
+            PetscReal x = (i - 0.5 * (nx - 1)) * dx;
+            PetscReal y = (j - 0.5 * (ny - 1)) * dy;
+
+            nodes[j][i].E = E0 * std::exp(-(x * x + y * y) / (2 * sigma * sigma));
             nodes[j][i].M[0] = 0.0;
             nodes[j][i].M[1] = 0.0;
         }
     }
+
     // "unlock" the local_sol
     ierr = DMDAVecRestoreArray(da, local_sol, &nodes); CHKERRQ(ierr);
 
     // synchronization
     // local -> global: real cells' values are updated
-    // global -> local: ghost cells' values are updated
     ierr = DMLocalToGlobalBegin(da, local_sol, INSERT_VALUES, solution); CHKERRQ(ierr);
     ierr = DMLocalToGlobalEnd(da, local_sol, INSERT_VALUES, solution); CHKERRQ(ierr);
+    
+    // global -> local: ghost cells' values are updated
     ierr = DMGlobalToLocalBegin(da, solution, INSERT_VALUES, local_sol); CHKERRQ(ierr);
     ierr = DMGlobalToLocalEnd(da, solution, INSERT_VALUES, local_sol); CHKERRQ(ierr);
 
@@ -83,5 +108,6 @@ int main(int argc, char **argv) {
     ierr = VecDestroy(&local_sol); CHKERRQ(ierr);
     ierr = DMDestroy(&da); CHKERRQ(ierr);
     ierr = PetscFinalize(); CHKERRQ(ierr);
+
     return 0;
 }
--- work/2dflow_plot.py
+++ work/2dflow_plot.py
@@ -1,72 +1,76 @@
-# Note: This file has a code for both 2d and 3d plots. The 3d plots are still work in progress
-# 2d works fine
+# Note: This file has a code for both 2d and 3d plots
+# Uncomment and use whichever you want
 
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 #from mpl_toolkits.mplot3d import Axes3D
 
-################## 2D plot #########################
+# ################## 2D plot #########################
 
-# Read HDF5 data
-with h5py.File('energy_out.h5', 'r') as f:
-    dataset = f['Energy'][:]
-    energy_data = dataset[:, :, 0]  # First component is energy
-
-# NOTe TO SELF: using a json file connect the grid specifications with both cpp and py files 
-nx, ny = 64, 64
-Lx, Ly = 1.0, 1.0
-x = np.linspace(-Lx/2, Lx/2, nx)
-y = np.linspace(-Ly/2, Ly/2, ny)
-X, Y = np.meshgrid(x, y)
-
-# heatmap of energy E
-plt.figure(figsize=(10, 8))
-heatmap = plt.imshow(energy_data, 
-                    extent=[-Lx/2, Lx/2, -Ly/2, Ly/2],
-                    cmap='viridis',
-                    origin='lower')
-
-
-cbar = plt.colorbar(heatmap, label='Energy (E)')
-cbar.set_label('Energy (E)', rotation=270, labelpad=20)
-
-# Axis labels and title
-plt.xlabel('X Position')
-plt.ylabel('Y Position')
-plt.title('2D Gaussian Energy Distribution')
-
-# Save and show
-# plt.savefig('2d_gaussian.png', dpi=300, bbox_inches='tight')
-plt.show()
-
-
-
-
-# ###################### 3D plot work in progress ######################
-# with h5py.File('energy_out.h5', 'r') as f:
+# # Read HDF5 data
+# with h5py.File('/home/jay/Desktop/Research/2dflow/build/temp_energy_out.h5', 'r') as f:
 #     dataset = f['Energy'][:]
 #     energy_data = dataset[:, :, 0]  # First component is energy
 
-# print("Data verification:")
-# print(f"Min: {np.min(energy_data):.4f}, Max: {np.max(energy_data):.4f}")
-# print(f"Center value: {energy_data[32,32]:.4f} (should be ~1.0)")
-
+# # NOTe TO SELF: using a json file connect the grid specifications with both cpp and py files 
 # nx, ny = 64, 64
-# x = np.linspace(-0.5, 0.5, nx)
-# y = np.linspace(-0.5, 0.5, ny)
+# Lx, Ly = 1.0, 1.0
+# x = np.linspace(-Lx/2, Lx/2, nx)
+# y = np.linspace(-Ly/2, Ly/2, ny)
 # X, Y = np.meshgrid(x, y)
 
-# fig = plt.figure(figsize=(12, 8))
-# ax = fig.add_subplot(111, projection='3d')
-# surf = ax.plot_surface(X, Y, energy_data, cmap='viridis', 
-#                       rstride=1, cstride=1, antialiased=True)
+# # heatmap of energy E
+# plt.figure(figsize=(10, 8))
+# heatmap = plt.imshow(energy_data, 
+#                     extent=[-Lx/2, Lx/2, -Ly/2, Ly/2],
+#                     cmap='viridis',
+#                     origin='lower')
 
-# ax.set_xlabel('X Position')
-# ax.set_ylabel('Y Position')
-# ax.set_zlabel('Energy Density')
-# plt.title('3D Gaussian Energy Distribution')
-# fig.colorbar(surf, shrink=0.5, aspect=10)
 
-# #plt.savefig('gaussian_3d.png', dpi=300, bbox_inches='tight')
+# cbar = plt.colorbar(heatmap, label='Energy (E)')
+# cbar.set_label('Energy (E)', rotation=270, labelpad=20)
+
+# # Axis labels and title
+# plt.xlabel('X Position')
+# plt.ylabel('Y Position')
+# plt.title('2D Gaussian Energy Distribution')
+
+# # Save and show
+# # plt.savefig('2d_gaussian.png', dpi=300, bbox_inches='tight')
 # plt.show()
+
+
+
+
+###################### 3D plot ######################
+with h5py.File('/home/jay/Desktop/Research/2dflow/build/energy_out.h5', 'r') as f:
+    dataset = f['Energy'][:]
+    energy_data = dataset[:, :, 0].T  # Transpose for correct X-Y orientation
+    
+    print("Data verification:")
+    print(f"Min: {np.min(energy_data):.4f}, Max: {np.max(energy_data):.4f}")
+    print(f"Center value: {energy_data[32,32]:.4f} (expected ~1.0)")
+    
+    nx, ny = 64, 64
+    x = np.linspace(-0.5, 0.5, nx)
+    y = np.linspace(-0.5, 0.5, ny)
+    X, Y = np.meshgrid(x, y)
+
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    surf = ax.plot_surface(X, Y, energy_data, 
+                          cmap='viridis',
+                          vmin=0, vmax=1.0,
+                          rstride=1, cstride=1,
+                          antialiased=True)
+    
+    ax.set_zlim(0, 1.1)
+    ax.set_xlabel('X Position')
+    ax.set_ylabel('Y Position')
+    ax.set_zlabel('Energy Density')
+    plt.title('3D Gaussian Energy Distribution')
+    
+    fig.colorbar(surf, shrink=0.5, aspect=10, label='Energy')
+    plt.show()
