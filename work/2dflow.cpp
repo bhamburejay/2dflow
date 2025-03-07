8d441f1fdcf363bb3865d7f9517e05ff822972ff
Jay Bhambure <jay.bhambure@stonybrook.edu>
2025-03-06 22:36:15 -0500

    time stepped added

--- work/2dflow.cpp
+++ work/2dflow.cpp
@@ -2,55 +2,101 @@
 // The output of this file is stored in enery_out.h5 in build (NOTE TO SELF: move this to some place else)
 // 2dflow_plot.py takes the enery_out.h5 file as input to produce plots
 
+#include <petsc.h>
 #include <iostream>
 #include <fstream>
-#include <cmath>
-#include <mpi.h>
-#include <petsc.h>
-#include <petscdm.h>
-#include <petscdmda.h>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+#include "VischydroNode.hpp"
 #include <petscviewerhdf5.h>
 #include <json/json.h>
-#include "VischydroNode.hpp"
+
+/* Diff eqn is de/dt = -(4/3)(e/t) RHS funtion well calculates RHS and is called at each timestep*/
+PetscErrorCode RHSFunction(TS ts, PetscReal t, Vec E, Vec F, void *ctx) {
+    const PetscScalar *e;
+    PetscScalar *f;
+    PetscErrorCode ierr;
+    PetscInt size;
+
+    ierr = VecGetArrayRead(E, &e); CHKERRQ(ierr);
+    ierr = VecGetArray(F, &f); CHKERRQ(ierr);
+    ierr = VecGetSize(E, &size); CHKERRQ(ierr);
+
+    // calculating RHS at every grid point
+    for (PetscInt i = 0; i < size; i++) {
+        f[i] = -4.0/3.0*e[i]/t;
+    }
+
+    ierr = VecRestoreArrayRead(E, &e); CHKERRQ(ierr);
+    ierr = VecRestoreArray(F, &f); CHKERRQ(ierr);
+    return 0;
+}
+
+// Saving data to HDF5. NOTE TO SELF: Need TO SELF restructure this part
+void SaveSolution(Vec E, PetscReal t) {
+    PetscViewer viewer;
+    const PetscFileMode mode = (t == 0.0) ? FILE_MODE_WRITE : FILE_MODE_APPEND;
+    PetscViewerHDF5Open(PETSC_COMM_WORLD, "energy_out.h5", mode, &viewer);
+    
+    std::stringstream dataset_name;
+    dataset_name << "Energy_t_" << std::fixed << std::setprecision(4) << t;
+    PetscObjectSetName((PetscObject)E, dataset_name.str().c_str());
+    
+    VecView(E, viewer);
+    PetscViewerDestroy(&viewer);
+}
+
+// Monitoring & save the solution at each time step
+PetscErrorCode Monitor(TS ts, PetscInt step, PetscReal t, Vec E, void *ctx) {
+    PetscFunctionBeginUser;
+    SaveSolution(E, t);
+    PetscFunctionReturn(0);
+}
 
 int main(int argc, char **argv) {
     PetscErrorCode ierr;
-    PetscMPIInt rank;
-    DM da;                                  // 2d grid object
+    PetscMPIInt rank, size;
+    DM da;                                      // 2D grid object
     Vec solution, local_sol;
-    VischydroNode **nodes;                  // Class that defined required nodes
-    PetscInt nx, ny;                        // latttice size
-    PetscReal Lx, Ly, E0, sigma, dx, dy;    // physical size, energy amplitude, std dev, grid spacing
-    
+    VischydroNode **nodes;                      // Class that defines required nodes
+    PetscInt nx, ny;                            // lattice size
+    PetscReal Lx, Ly, E0, sigma, dx, dy;        // physical size, energy amplitude, std dev, grid spacing
+    PetscScalar t_start, t_end, dt;             // time start and end
+    TS ts;                                      // time stepper object from PETSc
+
     ierr = PetscInitialize(&argc, &argv, NULL, NULL); CHKERRQ(ierr);
     ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank); CHKERRQ(ierr);
+    ierr = MPI_Comm_size(PETSC_COMM_WORLD, &size); CHKERRQ(ierr);
 
-    // Json setup
-    Json::Value config;
-    std::ifstream config_file("2dflow_input.json");
+    // inputuring JSON input file 
+    Json::Value input;
+    std::ifstream input_file("2dflow_input.json");
     Json::CharReaderBuilder reader;
     std::string errs;
-
-    if (!Json::parseFromStream(reader, config_file, &config, &errs)) {
+    
+    if (!Json::parseFromStream(reader, input_file, &input, &errs)) {
         std::cerr << "JSON parse error: " << errs << std::endl;
         PetscFinalize();
         return 1;
     }
 
-    // Extract parameters from JSON
-    nx = config["grid"]["nx"].asInt();
-    ny = config["grid"]["ny"].asInt();
-    static const int stencil_width = config["grid"]["stencil_width"].asInt();
-    Lx = config["physical_size"]["Lx"].asDouble();
-    Ly = config["physical_size"]["Ly"].asDouble();
-    E0 = config["initial_conditions"]["amplitude"].asDouble();
-    sigma = config["initial_conditions"]["sigma"].asDouble();
-
+    // Extracting parameters from JSON input file
+    nx = input["grid"]["nx"].asInt();
+    ny = input["grid"]["ny"].asInt();
+    static const int stencil_width = input["grid"]["stencil_width"].asInt();
+    Lx = input["physical_size"]["Lx"].asDouble();
+    Ly = input["physical_size"]["Ly"].asDouble();
+    E0 = input["initial_conditions"]["amplitude"].asDouble();
+    sigma = input["initial_conditions"]["sigma"].asDouble();
+    t_start = input["time_settings"]["dt"].asDouble();
+    t_end = input["time_settings"]["dt"].asDouble();
+    dt = input["time_settings"]["dt"].asDouble();
     dx = Lx / (nx - 1);
     dy = Ly / (ny - 1);
 
-    // 2d grid with ghosted boundary conditions -> this means, the values of the real boundary cells 
-    // are copied to ghost cells, micking neumann boundary conditions
+    /*2d grid with ghosted boundary conditions -> this means, the values of the real boundary cells 
+    are copied to ghost cells, micking neumann boundary conditions */
     ierr = DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, DM_BOUNDARY_GHOSTED,
                         DMDA_STENCIL_BOX, nx, ny, PETSC_DECIDE, PETSC_DECIDE,
                         VischydroNode::NDOF, stencil_width,
@@ -59,8 +105,8 @@ int main(int argc, char **argv) {
     ierr = DMSetUp(da); CHKERRQ(ierr);
 
     // Set coordinates
-    ierr = DMDASetUniformCoordinates(da, -Lx / 2, Lx / 2, -Ly / 2, Ly / 2, 0, 0); CHKERRQ(ierr);
-    
+    ierr = DMDASetUniformCoordinates(da, -Lx/2, Lx/2, -Ly/2, Ly/2, 0, 0); CHKERRQ(ierr);
+
     // global_vec (solutions) to store solution of diff. eqn. and 
     // local_vec (local_sol) part of global_vec to perform distributed computing at each process
     ierr = DMCreateGlobalVector(da, &solution); CHKERRQ(ierr);
@@ -69,16 +115,13 @@ int main(int argc, char **argv) {
     // capture local grid info in a given process
     PetscInt xs, ys, xm, ym;
     ierr = DMDAGetCorners(da, &xs, &ys, NULL, &xm, &ym, NULL); CHKERRQ(ierr);
-
-    // "lock" the local grid info in local_sol to populate /update it
     ierr = DMDAVecGetArray(da, local_sol, &nodes); CHKERRQ(ierr);
 
     for (PetscInt j = ys; j < ys + ym; j++) {
         for (PetscInt i = xs; i < xs + xm; i++) {
             PetscReal x = (i - 0.5 * (nx - 1)) * dx;
             PetscReal y = (j - 0.5 * (ny - 1)) * dy;
-
-            nodes[j][i].E = E0 * std::exp(-(x * x + y * y) / (2 * sigma * sigma));
+            nodes[j][i].E = E0 * std::exp(-(x*x + y*y)/(2*sigma*sigma));
             nodes[j][i].M[0] = 0.0;
             nodes[j][i].M[1] = 0.0;
         }
@@ -91,19 +134,33 @@ int main(int argc, char **argv) {
     // local -> global: real cells' values are updated
     ierr = DMLocalToGlobalBegin(da, local_sol, INSERT_VALUES, solution); CHKERRQ(ierr);
     ierr = DMLocalToGlobalEnd(da, local_sol, INSERT_VALUES, solution); CHKERRQ(ierr);
-    
+
     // global -> local: ghost cells' values are updated
     ierr = DMGlobalToLocalBegin(da, solution, INSERT_VALUES, local_sol); CHKERRQ(ierr);
     ierr = DMGlobalToLocalEnd(da, solution, INSERT_VALUES, local_sol); CHKERRQ(ierr);
 
-    // save data to HDF5
-    PetscViewer viewer;
-    ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD, "energy_out.h5", FILE_MODE_WRITE, &viewer); CHKERRQ(ierr);
-    ierr = PetscObjectSetName((PetscObject)solution, "Energy"); CHKERRQ(ierr);
-    ierr = VecView(solution, viewer); CHKERRQ(ierr);
-    ierr = PetscViewerDestroy(&viewer); CHKERRQ(ierr);
+    // Create time stepper context
+    ierr = TSCreate(PETSC_COMM_WORLD, &ts); CHKERRQ(ierr);
+    ierr = TSSetType(ts, TSEULER); CHKERRQ(ierr);
+    ierr = TSSetRHSFunction(ts, NULL, RHSFunction, NULL); CHKERRQ(ierr);
+    ierr = TSSetSolution(ts, solution); CHKERRQ(ierr);
 
-    // cleanup
+
+    // NOTE TO SELF: Get all these inputs from JSON. While doing so there are some errors.
+    PetscReal initial_time = 1.0e-1;
+    ierr = TSSetTime(ts, initial_time); CHKERRQ(ierr);
+    ierr = TSSetTimeStep(ts, 0.01); CHKERRQ(ierr);
+    ierr = TSSetMaxTime(ts, 1.0); CHKERRQ(ierr);
+
+    // Set monitor function and save initial condition
+    ierr = TSMonitorSet(ts, Monitor, NULL, NULL); CHKERRQ(ierr);
+    SaveSolution(solution, 0.0);
+
+    // Solve the ODE
+    ierr = TSSolve(ts, solution); CHKERRQ(ierr);
+
+    // Cleanup
+    ierr = TSDestroy(&ts); CHKERRQ(ierr);
     ierr = VecDestroy(&solution); CHKERRQ(ierr);
     ierr = VecDestroy(&local_sol); CHKERRQ(ierr);
     ierr = DMDestroy(&da); CHKERRQ(ierr);
--- work/2dflow_plot.py
+++ work/2dflow_plot.py
@@ -1,76 +1,47 @@
-# Note: This file has a code for both 2d and 3d plots
-# Uncomment and use whichever you want
-
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
 
-# ################## 2D plot #########################
+def plot_energy_vs_time(hdf5_path):
+    with h5py.File(hdf5_path, 'r') as f:
+        # Get sorted datasets by time
+        datasets = sorted(f.keys(), key=lambda x: float(x.split('_t_')[1]))
+        
+        # Initialize arrays
+        times = np.zeros(len(datasets))
+        avg_energy = np.zeros(len(datasets))
+        center_energy = np.zeros(len(datasets))
+        
+        for i, name in enumerate(datasets):
+            times[i] = float(name.split('_t_')[1])
+            data = f[name][:].squeeze()
+            
+            # Handle 3D data (time, y, x)
+            if data.ndim == 3:
+                # Take first time slice and find 2D center
+                slice_data = data[0]
+                ny, nx = slice_data.shape
+                avg_energy[i] = np.mean(slice_data)
+                center_energy[i] = slice_data[ny//2, nx//2]
+            elif data.ndim == 2:
+                # Standard 2D case
+                ny, nx = data.shape
+                avg_energy[i] = np.mean(data)
+                center_energy[i] = data[ny//2, nx//2]
+            else:
+                # Handle 1D case
+                avg_energy[i] = np.mean(data)
+                center_energy[i] = data[len(data)//2]
 
-# # Read HDF5 data
-# with h5py.File('/home/jay/Desktop/Research/2dflow/build/temp_energy_out.h5', 'r') as f:
-#     dataset = f['Energy'][:]
-#     energy_data = dataset[:, :, 0]  # First component is energy
-
-# # NOTe TO SELF: using a json file connect the grid specifications with both cpp and py files 
-# nx, ny = 64, 64
-# Lx, Ly = 1.0, 1.0
-# x = np.linspace(-Lx/2, Lx/2, nx)
-# y = np.linspace(-Ly/2, Ly/2, ny)
-# X, Y = np.meshgrid(x, y)
-
-# # heatmap of energy E
-# plt.figure(figsize=(10, 8))
-# heatmap = plt.imshow(energy_data, 
-#                     extent=[-Lx/2, Lx/2, -Ly/2, Ly/2],
-#                     cmap='viridis',
-#                     origin='lower')
-
-
-# cbar = plt.colorbar(heatmap, label='Energy (E)')
-# cbar.set_label('Energy (E)', rotation=270, labelpad=20)
-
-# # Axis labels and title
-# plt.xlabel('X Position')
-# plt.ylabel('Y Position')
-# plt.title('2D Gaussian Energy Distribution')
-
-# # Save and show
-# # plt.savefig('2d_gaussian.png', dpi=300, bbox_inches='tight')
-# plt.show()
-
-
-
-
-###################### 3D plot ######################
-with h5py.File('/home/jay/Desktop/Research/2dflow/build/energy_out.h5', 'r') as f:
-    dataset = f['Energy'][:]
-    energy_data = dataset[:, :, 0].T  # Transpose for correct X-Y orientation
-    
-    print("Data verification:")
-    print(f"Min: {np.min(energy_data):.4f}, Max: {np.max(energy_data):.4f}")
-    print(f"Center value: {energy_data[32,32]:.4f} (expected ~1.0)")
-    
-    nx, ny = 64, 64
-    x = np.linspace(-0.5, 0.5, nx)
-    y = np.linspace(-0.5, 0.5, ny)
-    X, Y = np.meshgrid(x, y)
-
-    fig = plt.figure(figsize=(12, 8))
-    ax = fig.add_subplot(111, projection='3d')
-    
-    surf = ax.plot_surface(X, Y, energy_data, 
-                          cmap='viridis',
-                          vmin=0, vmax=1.0,
-                          rstride=1, cstride=1,
-                          antialiased=True)
-    
-    ax.set_zlim(0, 1.1)
-    ax.set_xlabel('X Position')
-    ax.set_ylabel('Y Position')
-    ax.set_zlabel('Energy Density')
-    plt.title('3D Gaussian Energy Distribution')
-    
-    fig.colorbar(surf, shrink=0.5, aspect=10, label='Energy')
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.plot(times, avg_energy, 'b-', label='Average Energy')
+    plt.plot(times, center_energy, 'r--', label='Center Energy')
+    plt.xlabel('Time')
+    plt.ylabel('Energy')
+    plt.title('Energy vs Time')
+    plt.legend()
+    plt.grid(True)
     plt.show()
+
+plot_energy_vs_time('energy_out.h5')
