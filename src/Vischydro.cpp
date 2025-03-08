76a7390a4f90d8ca4acbd55244f5430d7d1a617f
derek-teaney <derek.teaney@stonybrook.edu>
2025-03-08 12:25:49 -0500

    updated test gauss

--- src/Vischydro.cpp
+++ src/Vischydro.cpp
@@ -1,9 +1,6 @@
 #include "Vischydro.hpp"
 #include "EOS.hpp"
 
-PetscErrorCode EulerRHSFunction(TS ts, PetscReal t, Vec U, Vec G, void *ctx) ;
-
-/* Diff eqn is de/dt = -(4/3)(e/t) RHS funtion well calculates RHS and is called at each timestep*/
 PetscErrorCode EulerRHSFunction(TS ts, PetscReal t, Vec U, Vec G, void *ctx) {
   const Vischydro &run = *(Vischydro *)ctx;
   auto eos = run.eos;
@@ -17,7 +14,7 @@ PetscErrorCode EulerRHSFunction(TS ts, PetscReal t, Vec U, Vec G, void *ctx) {
   DMDAVecGetArray(run.domain, run.local_solution, &asol);
   VischydroNode **ag;
   DMDAVecGetArray(run.domain, G, &ag);
-  
+
   // Loop over grid points and calculate RHS
   PetscInt xs, ys, xm, ym;
   VecZeroEntries(G);
@@ -27,7 +24,7 @@ PetscErrorCode EulerRHSFunction(TS ts, PetscReal t, Vec U, Vec G, void *ctx) {
       // Calculate RH
       double pr = eos->get_pressure(asol[j][i].E, asol[j][i].Mnrm());
       double E = asol[j][i].E;
-      ag[j][i].E = -  (E + pr) / t;
+      ag[j][i].E = -(E + pr) / t;
       ag[j][i].M[0] = 0.0;
       ag[j][i].M[1] = 0.0;
     }
@@ -37,44 +34,45 @@ PetscErrorCode EulerRHSFunction(TS ts, PetscReal t, Vec U, Vec G, void *ctx) {
   DMDAVecRestoreArray(run.domain, run.local_solution, &asol);
   DMDAVecRestoreArray(run.domain, G, &ag);
   return 0;
-
 }
 
-Vischydro::Vischydro(Json::Value &config, const EOS *eosin) : configuration(config), eos(eosin) {
+Vischydro::Vischydro(Json::Value &config, const EOS *eosin)
+    : configuration(config), eos(eosin) {
   // Extract parameters from JSON
-  int nx = configuration["grid"]["nx"].asInt();
-  int ny = configuration["grid"]["ny"].asInt();
-  static const int stencil_width = configuration["grid"]["stencil_width"].asInt();
-  double Lx = configuration["physical_size"]["Lx"].asDouble();
-  double Ly = configuration["physical_size"]["Ly"].asDouble();
+  // Change these lines to get_input()
+  nx = get_input({"grid", "nx"}).asInt();
+  ny = get_input({"grid", "ny"}).asInt();
+  xmin = get_input({"grid", "xmin"}).asDouble();
+  xmax = get_input({"grid", "xmax"}).asDouble();
+  ymin = get_input({"grid", "ymin"}).asDouble();
+  ymax = get_input({"grid", "ymax"}).asDouble();
+
+  double Lx = xmax - xmin;
+  double Ly = ymax - ymin;
 
   dx = Lx / (nx - 1);
   dy = Ly / (ny - 1);
 
-  int ierr ;
+  const int stencil_width = 2;
   // 2d grid with ghosted boundary conditions
-  ierr = DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, DM_BOUNDARY_GHOSTED,
-                      DMDA_STENCIL_BOX, nx, ny, PETSC_DECIDE, PETSC_DECIDE,
-                      VischydroNode::NDOF, stencil_width,
-                      NULL, NULL, &domain); 
-  ierr = DMSetFromOptions(domain); 
-  ierr = DMSetUp(domain); 
+  DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, DM_BOUNDARY_GHOSTED,
+               DMDA_STENCIL_BOX, nx, ny, PETSC_DECIDE, PETSC_DECIDE,
+               VischydroNode::NDOF, stencil_width, NULL, NULL, &domain);
+  DMSetFromOptions(domain);
+  DMSetUp(domain);
+  DMCreateGlobalVector(domain, &solution);
+  DMCreateLocalVector(domain, &local_solution);
 
   // Set coordinates
-  ierr = DMDASetUniformCoordinates(domain, -Lx / 2, Lx / 2, -Ly / 2, Ly / 2, 0, 0); 
-
-  // global_vec (solutions) to store solution of diff. eqn. and 
-  // local_vec (local_sol) part of global_vec to perform distributed computing at each process
-  ierr = DMCreateGlobalVector(domain, &solution); 
-  ierr = DMCreateLocalVector(domain, &local_solution); 
+  DMDASetUniformCoordinates(domain, xmin, xmax, ymin, ymax, 0.0, 0.0);
+  DMGetCoordinates(domain, &coordinates);
+  DMGetCoordinateDM(domain, &cdomain);
 
   TSCreate(PETSC_COMM_WORLD, &stepper);
-  TSSetApplicationContext(stepper, this) ;
-  TSSetDM(stepper, domain); 
+  TSSetApplicationContext(stepper, this);
+  TSSetDM(stepper, domain);
   TSSetType(stepper, TSEULER);
   TSSetSolution(stepper, solution);
   TSSetRHSFunction(stepper, NULL, EulerRHSFunction, this);
+  TSSetFromOptions(stepper);
 }
-
-
-
--- src/test_json.cpp
+++ src/test_json.cpp
@@ -1,17 +1,40 @@
 #include "json/json.h"
 #include <iostream>
 
+Json::Value checkin(Json::Value &value){
+    if (value.isNull()) {
+        std::cerr << "Key not found in inputs" << std::endl;
+    }
+    return value;
+}   
+
 int main(int argc, char** argv) {
     Json::Value root;
     Json::Reader reader;
     Json::StyledWriter writer;
 
-    std::string json_str = "{\"name\":\"test\",\"age\":20}";
+    std::string json_str = "{\"name\":\"test\",\"age\":20,\"nextlevel\": {\"name\":\"test2\",\"age\":21}}";
+
     if (reader.parse(json_str, root)) {
         std::cout << writer.write(root) << std::endl;
     } else {
         std::cout << "parse error" << std::endl;
     }
+    Json::Value root2 = root ;
+    root2["nextlevel"]["name"]  = "test3" ;
+    std::cout << root2 << std::endl;
+    std::cout << root << std::endl;
+
+    // std::cout << get_input({"nextlevel", "name"}, root) << std::endl;
+    // std::cout << get_input({"nextlevel2", "name"}, root) << std::endl;
+    // std::cout << get_input({"nextlevel", "name2"}, root) << std::endl;
+
+    std::cout << checkin(root["nextlevel"]["name"]).asString() << std::endl;
+    std::cout << checkin(root["nextlevel"]["name2"]).asString() << std::endl;
+
+
+
+
 
     return 0;
 }
